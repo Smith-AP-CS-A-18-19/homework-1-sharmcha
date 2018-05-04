7f86af02a08cc95e6e657cbd3fdb5433e7cdb3db
Chase Smith <mcsmith01@alumni.amherst.edu>
2018-05-04 12:37:08 -0400

    Updated comments to 80

--- Homework1.java
+++ Homework1.java
@@ -1,7 +1,7 @@
 import java.awt.Rectangle;
 
 public class Homework1 {
-	
+
 	public static void main(String[] args) {
 		if (problem1() == -3) {
 			System.out.println("Pass 1");
@@ -19,29 +19,33 @@ public class Homework1 {
 			System.out.println("Pass 5");
 		}
 	}
-	
-	// Fix the error in this method so that it runs properly
+
+	/* Fix the error in this method so that it runs properly
+	 */
 	public static int problem1() {
 		int mystery = 1;
 		mystery = mystery + 1;
 		int mystery = 1 - 2 * mystery;
 		return mystery;
 	}
-	
-	// Use String methods to turn the provided String (input) into all uppercase letters and return the result
-	// Hint: The String class has a 'toUpperCase' method
-	
+
+	/* Use String methods to turn the provided String (input) into all uppercase
+	 * letters and return the result
+	 * Hint: The String class has a 'toUpperCase' method
+	 */
 	public static String problem2(String input) {
-		
+
 	}
 
-	// Use String methods to remove punctuation from the provided String (input)
-	// Hint: The String class has a 'replace' method
+	/* Use String methods to remove punctuation from the provided String (input)
+	 * Hint: The String class has a 'replace' method
+	 */
 	public static String problem3(String input) {
-		
+
 	}
-	
-	// Fix the errors in the following method
+
+	/* Fix the errors in the following method
+	 */
 	public static Rectangle problem4() {
 		Rectangle r1 = (5, 10, 15, 20);
 		double width = Rectangle(5, 10, 15, 20).getWidth();
@@ -51,10 +55,12 @@ public class Homework1 {
 		r3.translate("Far, far away");
 		return r3;
 	}
-	
-	// Use Rectangle methods to calculate and return the perimiter of the provided Rectangle (rect)
+
+	/* Use Rectangle methods to calculate and return the perimiter of the
+	 * provided Rectangle (rect)
+	 */
 	public static int problem5(Rectangle rect) {
 		
 	}
-	
+
 }
